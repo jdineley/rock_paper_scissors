388f925f68271fe7e484f382a1fc78b99f35d025
James Dineley <jdineley@hotmail.com>
2022-08-22 14:41:19 +0100

    Set up player / computer choice logic

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('connected')
+// console.log('connected')
+
+function rockPaperScissors(){
+    let options = ['rock', 'paper', 'scissors'];
+    let random = Math.floor(Math.random()*options.length);
+    console.log(random);
+    return options[random];
+}
+
+function getComputerChoice(){
+    return rockPaperScissors();
+}
+
+function getPlayerChoice(){
+    return rockPaperScissors();
+}
+
+// getComputerChoice();
+
+function playRound(playerSelection, computerSelection){
+    console.log(playerSelection(), computerSelection())
+}
+
+playRound(getComputerChoice, getPlayerChoice);
