055c1f86380ff91c5b3d27d22afc107cc8132bb1
James Dineley <jdineley@hotmail.com>
2022-08-22 16:32:25 +0100

    Full rock paper scissors best of 5 console game release A.1

--- app.js
+++ app.js
@@ -10,16 +10,13 @@ function getComputerChoice(){
     return rockPaperScissors();
 }
 
-function getPlayerChoice(){
-    return rockPaperScissors();
-}
 
-
-function playRound(getComputerChoice, getPlayerChoice){
-    let playerChoice = getPlayerChoice();
+function playRound(getComputerChoice, str){
+    let playerChoice = str.toLowerCase();
     let computerChoice = getComputerChoice();
     if(playerChoice === computerChoice){
         console.log('A draw, play again!');
+        return 'draw'
 
     } else {
         let gameArrayString = `${playerChoice} ${computerChoice}`
@@ -30,12 +27,39 @@ function playRound(getComputerChoice, getPlayerChoice){
             case 'paper rock':
             case 'rock scissors':
                 console.log(`Player choice: ${playerChoice}, Computer choice: ${computerChoice}: Player wins!!`)
+                return 'player wins'
             break;
             default:
                 console.log(`Player choice: ${playerChoice}, Computer choice: ${computerChoice}: Computer wins!!`)
+                return 'computer wins'
         }
     }
 
 }
 
-playRound(getComputerChoice, getPlayerChoice);
+
+function game() {
+    let curGame = {
+        player: 0,
+        computer: 0
+    }
+    while(curGame.player < 3 && curGame.computer < 3) {
+        let playerChoice = prompt('Make your choice now!')
+        switch(playRound(getComputerChoice, playerChoice)) {
+            case 'draw':
+                break;
+            case 'player wins':
+                curGame.player += 1
+                break;
+            case 'computer wins':
+                curGame.computer += 1
+                break;
+        }
+        console.log(curGame);     
+    }
+    if(curGame.player === 3){
+        console.log(`Player wins game!!  Final score: Player - ${curGame.player},  Computer - ${curGame.computer}`)
+    } else {
+        console.log(`Computer wins game!!  Final score: Computer - ${curGame.computer},  Player - ${curGame.player}`)
+    }
+}
