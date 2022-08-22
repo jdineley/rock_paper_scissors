e824758834515dc0ff079ae05610fffeb59fbce3
James Dineley <jdineley@hotmail.com>
2022-08-22 14:58:52 +0100

    Implement winner/loser logic #1

--- app.js
+++ app.js
@@ -17,8 +17,30 @@ function getPlayerChoice(){
 
 // getComputerChoice();
 
-function playRound(playerSelection, computerSelection){
-    console.log(playerSelection(), computerSelection())
+function playRound(getComputerChoice, getPlayerChoice){
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+    console.log(playerChoice, computerChoice);
+    if(playerChoice === computerChoice){
+        console.log('A draw, play again!');
+    } else if (playerChoice === 'rock' && computerChoice === 'scissors'){
+        console.log(`Player chose: ${playerChoice},  Computer chose: ${computerChoice}:  Player wins!`)
+    } 
+    else if (playerChoice === 'rock' && computerChoice === 'paper'){
+        console.log(`Player chose: ${playerChoice},  Computer chose: ${computerChoice}:  Computer wins!`)
+    } 
+    else if (playerChoice === 'paper' && computerChoice === 'scissors'){
+        console.log(`Player chose: ${playerChoice},  Computer chose: ${computerChoice}:  Computer wins!`)
+    } 
+    else if (playerChoice === 'paper' && computerChoice === 'rock'){
+        console.log(`Player chose: ${playerChoice},  Computer chose: ${computerChoice}:  Player wins!`)
+    } 
+    else if (playerChoice === 'scissors' && computerChoice === 'rock'){
+        console.log(`Player chose: ${playerChoice},  Computer chose: ${computerChoice}:  Computer wins!`)
+    } 
+    else if (playerChoice === 'scissors' && computerChoice === 'paper'){
+        console.log(`Player chose: ${playerChoice},  Computer chose: ${computerChoice}:  Player wins!`)
+    } 
 }
 
 playRound(getComputerChoice, getPlayerChoice);
