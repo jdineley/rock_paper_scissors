f4b88af7b9d16b0a908f584200bde28b396106df
James Dineley <jdineley@hotmail.com>
2022-08-22 15:55:39 +0100

    Refactored logic to use switch with fall through to capture multiple possible matches

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// console.log('connected')
 
 function rockPaperScissors(){
     let options = ['rock', 'paper', 'scissors'];
@@ -15,32 +14,28 @@ function getPlayerChoice(){
     return rockPaperScissors();
 }
 
-// getComputerChoice();
 
 function playRound(getComputerChoice, getPlayerChoice){
     let playerChoice = getPlayerChoice();
     let computerChoice = getComputerChoice();
-    console.log(playerChoice, computerChoice);
     if(playerChoice === computerChoice){
         console.log('A draw, play again!');
-    } else if (playerChoice === 'rock' && computerChoice === 'scissors'){
-        console.log(`Player chose: ${playerChoice},  Computer chose: ${computerChoice}:  Player wins!`)
-    } 
-    else if (playerChoice === 'rock' && computerChoice === 'paper'){
-        console.log(`Player chose: ${playerChoice},  Computer chose: ${computerChoice}:  Computer wins!`)
-    } 
-    else if (playerChoice === 'paper' && computerChoice === 'scissors'){
-        console.log(`Player chose: ${playerChoice},  Computer chose: ${computerChoice}:  Computer wins!`)
-    } 
-    else if (playerChoice === 'paper' && computerChoice === 'rock'){
-        console.log(`Player chose: ${playerChoice},  Computer chose: ${computerChoice}:  Player wins!`)
-    } 
-    else if (playerChoice === 'scissors' && computerChoice === 'rock'){
-        console.log(`Player chose: ${playerChoice},  Computer chose: ${computerChoice}:  Computer wins!`)
-    } 
-    else if (playerChoice === 'scissors' && computerChoice === 'paper'){
-        console.log(`Player chose: ${playerChoice},  Computer chose: ${computerChoice}:  Player wins!`)
-    } 
+
+    } else {
+        let gameArrayString = `${playerChoice} ${computerChoice}`
+        console.log(gameArrayString);
+
+        switch(gameArrayString) {
+            case 'scissors paper':
+            case 'paper rock':
+            case 'rock scissors':
+                console.log(`Player choice: ${playerChoice}, Computer choice: ${computerChoice}: Player wins!!`)
+            break;
+            default:
+                console.log(`Player choice: ${playerChoice}, Computer choice: ${computerChoice}: Computer wins!!`)
+        }
+    }
+
 }
 
 playRound(getComputerChoice, getPlayerChoice);
